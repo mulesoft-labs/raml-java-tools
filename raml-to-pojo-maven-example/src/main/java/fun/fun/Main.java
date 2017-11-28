d7f3c59d49fa8a7d804fbeca38d0a7ee1573625a
jpbelang <jpbelang@gmail.com>
2017-11-28 09:13:50 -0500

    Beginning a plugin....

---
+++ raml-to-pojo-maven-example/src/main/java/fun/fun/Main.java
@@ -0,0 +1,11 @@
+package fun.fun;
+
+/**
+ * Created. There, you have it.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ raml-to-pojo-maven-plugin/src/main/java/org/raml/ramltopojo/plugin/maven/RamlToPojoMojo.java
@@ -0,0 +1,120 @@
+/*
+ * Copyright 2013-2017 (c) MuleSoft, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied. See the License for the specific
+ * language governing permissions and limitations under the License.
+ */
+package org.raml.ramltopojo.plugin.maven;
+
+import org.apache.commons.io.FileUtils;
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
+import org.apache.maven.plugins.annotations.LifecyclePhase;
+import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
+import org.apache.maven.project.MavenProject;
+import org.raml.ramltopojo.RamlToPojo;
+import org.raml.ramltopojo.RamlToPojoBuilder;
+import org.raml.v2.api.RamlModelBuilder;
+import org.raml.v2.api.RamlModelResult;
+import org.raml.v2.api.model.common.ValidationResult;
+import org.raml.v2.api.model.v10.api.Api;
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
+import static org.apache.maven.plugins.annotations.ResolutionScope.COMPILE_PLUS_RUNTIME;
+import static org.raml.ramltopojo.TypeFetchers.fromAnywhere;
+import static org.raml.ramltopojo.TypeFinders.everyWhere;
+
+@Mojo(name = "generate", requiresProject = true, threadSafe = false, requiresDependencyResolution = COMPILE_PLUS_RUNTIME,
+        defaultPhase = LifecyclePhase.GENERATE_SOURCES)
+public class RamlToPojoMojo extends AbstractMojo {
+
+    @Parameter(defaultValue = "${project}")
+    private MavenProject project;
+
+    /**
+     * Skip plug-in execution.
+     */
+    @Parameter(property = "skip", defaultValue = "false")
+    private boolean skip;
+
+    /**
+     * Target directory for generated Java source files.
+     */
+    @Parameter(property = "outputDirectory",
+            defaultValue = "${project.build.directory}/generated-sources/raml-to-jaxrs-maven-plugin")
+    private File outputDirectory;
+
+    /**
+     * An array of locations of the RAML file(s).
+     */
+    @Parameter(property = "ramlFile", required = true)
+    private File ramlFile;
+
+    /**
+     * An array of locations of the RAML file(s).
+     */
+    @Parameter(property = "defaultPackage", required = true)
+    private String defaultPackage;
+
+
+    @Override
+    public void execute() throws MojoExecutionException, MojoFailureException {
+
+        getLog().error("GAAAAAA!");
+        if (skip) {
+            getLog().info("Skipping execution...");
+            return;
+        }
+
+        if (ramlFile == null) {
+            throw new MojoExecutionException("ramlFile is not defined");
+        }
+
+        try {
+            FileUtils.forceMkdir(outputDirectory);
+        } catch (final IOException ioe) {
+            throw new MojoExecutionException("Failed to create directory: " + outputDirectory, ioe);
+        }
+
+        try {
+            project.addCompileSourceRoot(outputDirectory.getPath());
+
+            RamlModelResult ramlModelResult =
+                    new RamlModelBuilder().buildApi(
+                            new FileReader(ramlFile),
+                            ".");
+            if (ramlModelResult.hasErrors()) {
+                for (ValidationResult validationResult : ramlModelResult.getValidationResults()) {
+                    System.err.println(validationResult.getMessage());
+                }
+                throw new MojoExecutionException("invalid raml " + ramlFile);
+            }
+
+            final Api api = ramlModelResult.getApiV10();
+            RamlToPojo ramlToPojo = RamlToPojoBuilder.builder(api)
+                    .inPackage(defaultPackage)
+                    .fetchTypes(fromAnywhere())
+                    .findTypes(everyWhere()).build();
+
+            ramlToPojo.buildPojos().createAllTypes(outputDirectory.getAbsolutePath());
+
+        } catch (IOException e) {
+
+            throw new MojoExecutionException("execution exception", e);
+        }
+    }
+}
