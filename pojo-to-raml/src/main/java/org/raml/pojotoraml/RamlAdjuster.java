c5489a9ea7970ddb96de025bc05985c58be73b64
jpbelang <jpbelang@gmail.com>
2018-01-24 21:50:02 -0500

    Quick check for parents.

--- pojo-to-raml/src/main/java/org/raml/pojotoraml/RamlAdjuster.java
+++ pojo-to-raml/src/main/java/org/raml/pojotoraml/RamlAdjuster.java
@@ -5,6 +5,7 @@ import org.raml.builder.TypeDeclarationBuilder;
 import org.raml.builder.TypePropertyBuilder;
 
 import java.lang.reflect.Type;
+import java.util.List;
 
 /**
  * Created. There, you have it.
@@ -41,6 +42,60 @@ public interface RamlAdjuster {
         }
     }
 
+    class Composite implements RamlAdjuster {
+
+        private final  List<RamlAdjuster> adjusters;
+
+        public Composite(List<RamlAdjuster> adjusters) {
+            this.adjusters = adjusters;
+        }
+
+        @Override
+        public String adjustEnumValue(Class<?> type, String name) {
+
+            String val = name;
+            for (RamlAdjuster adjuster : adjusters) {
+                val = adjuster.adjustEnumValue(type, val);
+            }
+            return val;
+        }
+
+        @Override
+        public TypeBuilder adjustType(Type type, TypeBuilder builder) {
+            TypeBuilder val = builder;
+            for (RamlAdjuster adjuster : adjusters) {
+                val = adjuster.adjustType(type, val);
+            }
+            return val;
+        }
+
+        @Override
+        public String adjustTypeName(Class<?> aClass, String name, ClassParser parser) {
+            String val = name;
+            for (RamlAdjuster adjuster : adjusters) {
+                val = adjuster.adjustTypeName(aClass, val, parser);
+            }
+            return val;
+        }
+
+        @Override
+        public TypePropertyBuilder adjustScalarProperty(TypeDeclarationBuilder typeDeclaration, Property property, TypePropertyBuilder typePropertyBuilder) {
+            TypePropertyBuilder val = typePropertyBuilder;
+            for (RamlAdjuster adjuster : adjusters) {
+                val = adjuster.adjustScalarProperty(typeDeclaration, property, val);
+            }
+            return val;
+        }
+
+        @Override
+        public TypePropertyBuilder adjustComposedProperty(TypeDeclarationBuilder typeDeclaration, Property property, TypePropertyBuilder typePropertyBuilder) {
+            TypePropertyBuilder val = typePropertyBuilder;
+            for (RamlAdjuster adjuster : adjusters) {
+                val = adjuster.adjustComposedProperty(typeDeclaration, property, val);
+            }
+            return val;
+        }
+    }
     String adjustEnumValue(Class<?> type, String name);
     TypeBuilder adjustType(Type type, TypeBuilder builder);
     String adjustTypeName(Class<?> aClass, String name, ClassParser parser);
