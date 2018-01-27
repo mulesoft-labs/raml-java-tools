b9f9fdd8dbfc869f4a44ae8e65a797c19215c22b
jpbelang <jpbelang@gmail.com>
2018-01-27 13:36:06 -0500

    Removed dead code.

--- raml-to-pojo/src/main/java/org/raml/ramltopojo/extensions/javadoc/JavadocObjectTypeHandlerPlugin.java
+++ raml-to-pojo/src/main/java/org/raml/ramltopojo/extensions/javadoc/JavadocObjectTypeHandlerPlugin.java
@@ -13,51 +13,6 @@ import org.raml.v2.api.model.v10.datamodel.TypeDeclaration;
  * Created. There, you have it.
  */
 
-/*
-
-  private interface JavadocAdder {
-
-    void addJavadoc(String format, Object... args);
-  }
-  @Override
-  public void onGetterMethodDeclaration(CurrentBuild currentBuild, final MethodSpec.Builder typeSpec,
-                                        TypeDeclaration typeDeclaration) {
-    if (typeDeclaration.description() != null) {
-      typeSpec.addJavadoc("$L\n", typeDeclaration.description().value());
-    }
-
-    javadocExamples(new JavadocAdder() {
-
-      @Override
-      public void addJavadoc(String format, Object... args) {
-        typeSpec.addJavadoc(format, args);
-      }
-    }, typeDeclaration);
-  }
-
-  public void javadocExamples(JavadocAdder adder, TypeDeclaration typeDeclaration) {
-    ExampleSpec example = typeDeclaration.example();
-    if (example != null) {
-
-      javadoc(adder, example);
-    }
-
-    for (ExampleSpec exampleSpec : typeDeclaration.examples()) {
-      javadoc(adder, exampleSpec);
-    }
-  }
-
-  public void javadoc(JavadocAdder adder, ExampleSpec exampleSpec) {
-    adder.addJavadoc("Example:\n");
-
-    if (exampleSpec.name() != null) {
-      adder.addJavadoc(" $L\n", exampleSpec.name());
-    }
-
-    adder.addJavadoc(" $L\n", "<pre>\n{@code\n" + exampleSpec.value() + "\n}</pre>");
-  }
-
- */
 public class JavadocObjectTypeHandlerPlugin extends ObjectTypeHandlerPlugin.Helper {
 
     private interface JavadocAdder {
