8bbf4b4930e18409f945653f3140891cf5c9116b
jpbelang <jpbelang@gmail.com>
2018-03-09 15:57:20 -0500

    Parser giving me some pain around: https://github.com/mulesoft-labs/raml-for-jax-rs/issues/337

--- raml-to-pojo/src/main/java/org/raml/ramltopojo/array/ArrayTypeHandler.java
+++ raml-to-pojo/src/main/java/org/raml/ramltopojo/array/ArrayTypeHandler.java
@@ -36,7 +36,17 @@ public class ArrayTypeHandler implements TypeHandler {
 
     @Override
     public TypeName javaClassReference(GenerationContext generationContext, EventType type) {
-        return ParameterizedTypeName.get(ClassName.get(List.class), TypeDeclarationType.calculateTypeName(typeDeclaration.items().name(), typeDeclaration.items(), generationContext, type).box());
+        String itemTypeName = typeDeclaration.items().name();
+        if ( "object".equals(itemTypeName)) {
+            itemTypeName = typeDeclaration.items().type();
+        }
+
+        if ( "object".equals(itemTypeName)) {
+
+            throw new GenerationException("unable to create type array item of type object (or maybe an inline array type ?)");
+        }
+
+        return ParameterizedTypeName.get(ClassName.get(List.class), TypeDeclarationType.calculateTypeName(itemTypeName, typeDeclaration.items(), generationContext, type).box());
     }
 
     @Override
