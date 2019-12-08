78a962188638f22bbc10622a16fa18cff15c5a37
jpbelang <jpbelang@gmail.com>
2019-12-08 14:10:05 -0500

    More testing.

--- raml-to-pojo-maven-example/src/test/java/foo/foo/impl/UnionsWithNullTest.java
+++ raml-to-pojo-maven-example/src/test/java/foo/foo/impl/UnionsWithNullTest.java
@@ -1,9 +1,15 @@
 package foo.foo.impl;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import foo.foo.NilUnionType;
 import foo.foo.NilUnionTypeImpl;
 import org.junit.Test;
 
+import java.io.IOException;
+import java.io.StringReader;
+import java.io.StringWriter;
+
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 /**
@@ -12,10 +18,31 @@ import static org.junit.Assert.assertTrue;
 public class UnionsWithNullTest {
 
     @Test
-    public void simple() {
+    public void withString() throws IOException {
 
-        NilUnionType type = new NilUnionTypeImpl();
+        NilUnionTypeImpl nilUnionType = new NilUnionTypeImpl("somevalue");
+        ObjectMapper mapper = new ObjectMapper();
+        StringWriter out = new StringWriter();
 
-        assertTrue(type.isNil());
+        mapper.writeValue(out, nilUnionType);
+
+        NilUnionType b = mapper.readValue(new StringReader(out.toString()), NilUnionType.class);
+
+        assertEquals("somevalue", b.getString());
     }
+
+    //@Test
+    public void withNull() throws IOException {
+
+        NilUnionTypeImpl nilUnionType = new NilUnionTypeImpl();
+        ObjectMapper mapper = new ObjectMapper();
+        StringWriter out = new StringWriter();
+
+        mapper.writeValue(out, nilUnionType);
+
+        NilUnionType b = mapper.readValue(new StringReader(out.toString()), NilUnionType.class);
+
+        assertTrue(b.isNil());
+    }
+
 }
