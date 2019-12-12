b2cccb2934d59af1034bff3814648f7220a5a36d
Philipp Kaiser <philipp.kaiser@perfectpattern.de>
2019-12-12 13:41:43 +0100

    Fixed wrong timezone test

--- raml-to-pojo-maven-example/src/test/java/foo/foo/impl/UnionsWithDatesTest.java
+++ raml-to-pojo-maven-example/src/test/java/foo/foo/impl/UnionsWithDatesTest.java
@@ -3,11 +3,13 @@ package foo.foo.impl;
 import static org.junit.Assert.assertTrue;
 
 import java.io.IOException;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 import foo.foo.Date1UnionImpl;
 import foo.foo.Date2UnionImpl;
 import foo.foo.Date3UnionImpl;
+import foo.foo.Date4UnionImpl;
 import foo.foo.DateUnion;
 import foo.foo.DateUnionImpl;
 
@@ -25,20 +27,23 @@ public class UnionsWithDatesTest {
         dateUnion.setDate1(new Date1UnionImpl(new Date(1234567)));
         dateUnion.setDate2(new Date2UnionImpl(new Date(1234567)));
         dateUnion.setDate3(new Date3UnionImpl(new Date(1234567)));
+        dateUnion.setDate4(new Date4UnionImpl(new Date(1234567)));
         
         ObjectMapper mapper = new ObjectMapper();
         String json = mapper.writeValueAsString(dateUnion);
                         
         JsonNode node = mapper.readTree(json);
-        
-        assertTrue(node.get("date1").isTextual() && node.get("date1").asText().equals("1970-20-01"));
-        assertTrue(node.get("date2").isTextual() && node.get("date2").asText().equals("01:20:34"));
-        assertTrue(node.get("date3").isTextual() && node.get("date3").asText().equals("1970-01-01T01:20:34"));
+        assertTrue(node.get("date1").isTextual() && node.get("date1").asText().equals(new SimpleDateFormat("yyyy-mm-dd").format(new Date(1234567))));
+        assertTrue(node.get("date2").isTextual() && node.get("date2").asText().equals(new SimpleDateFormat("hh:mm:ss").format(new Date(1234567))));
+        assertTrue(node.get("date3").isTextual() && node.get("date3").asText().equals(new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss").format(new Date(1234567))));
+        assertTrue(node.get("date4").isTextual() && node.get("date4").asText().equals(new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssZ").format(new Date(1234567))));
+
         
         DateUnion dateUnionTemp = mapper.readValue(json, DateUnion.class);
         
         assertTrue(dateUnionTemp.getDate1().isDate());
         assertTrue(dateUnionTemp.getDate2().isDate());
         assertTrue(dateUnionTemp.getDate3().isDate());
+        assertTrue(dateUnionTemp.getDate4().isDate());
     }
 }
