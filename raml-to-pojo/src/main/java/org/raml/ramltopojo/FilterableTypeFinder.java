38bcaf95eb31692872ebf9e5abbea68c53c8fe10
jpbelang <jpbelang@gmail.com>
2020-04-16 09:10:49 -0400

    Almost done, need libraries....

--- raml-to-pojo/src/main/java/org/raml/ramltopojo/FilterableTypeFinder.java
+++ raml-to-pojo/src/main/java/org/raml/ramltopojo/FilterableTypeFinder.java
@@ -1,10 +1,7 @@
 package org.raml.ramltopojo;
 
 import amf.client.model.document.Document;
-import amf.client.model.domain.AnyShape;
-import amf.client.model.domain.WebApi;
-
-import java.util.stream.Collectors;
+import amf.client.model.domain.*;
 
 /**
  * Created. There, you have it.
@@ -23,6 +20,38 @@ public class FilterableTypeFinder {
                 .map(x -> (AnyShape) x)
                 .filter(s -> filterCallBack.filter(namedElementPath))
                 .forEach(s -> foundCallback.found(namedElementPath, s));
+
+        WebApi encoded = (WebApi) api.encodes();
+        for (EndPoint endPoint : encoded.endPoints()) {
+
+            for (Parameter p : endPoint.parameters()) {
+                foundCallback.found(namedElementPath.append(endPoint, p), (AnyShape) p.schema());
+            }
+
+            for ( Operation operation : endPoint.operations()) {
+
+                Request request = operation.request();
+                for (Parameter p : request.queryParameters()) {
+                    foundCallback.found(namedElementPath.append(endPoint, operation, p), (AnyShape) p.schema());
+                }
+
+                for (Parameter p : request.headers()) {
+                    foundCallback.found(namedElementPath.append(endPoint, operation, p), (AnyShape) p.schema());
+                }
+
+                for ( Response response: operation.responses()) {
+
+                    for (Parameter p : response.headers()) {
+                        foundCallback.found(namedElementPath.append(endPoint, operation, response, p), (AnyShape) p.schema());
+                    }
+
+                    for (Payload payload: response.payloads()) {
+                        foundCallback.found(namedElementPath.append(endPoint, operation, response, payload), (AnyShape) payload.schema());
+                    }
+                }
+            }
+        }
+
 
         ((WebApi) api.encodes()).endPoints().stream()
                 .map(e -> e.operations())
--- raml-to-pojo/src/main/java/org/raml/ramltopojo/NamedElementPath.java
+++ raml-to-pojo/src/main/java/org/raml/ramltopojo/NamedElementPath.java
@@ -1,9 +1,14 @@
 package org.raml.ramltopojo;
 
+import amf.client.model.domain.EndPoint;
+import amf.client.model.domain.NamedDomainElement;
+import amf.client.model.domain.Operation;
+import amf.client.model.domain.Parameter;
 import amf.core.model.domain.DomainElement;
-import amf.core.model.domain.NamedDomainElement;
 import lombok.AllArgsConstructor;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
@@ -17,5 +22,12 @@ public class NamedElementPath {
 
     public static NamedElementPath root() {
         return new NamedElementPath(Collections.emptyList());
+    }
+
+    public <T extends NamedDomainElement> NamedElementPath append(T... elements) {
+
+        ArrayList<NamedDomainElement> domainElements = new ArrayList<>(this.domainElements);
+        domainElements.addAll(Arrays.asList(elements));
+        return new NamedElementPath(domainElements);
     }
 }
